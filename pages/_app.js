5c9a1e6c49a056fe158bf307399f59cc29f5f736
ulrichwiseempire <ulrichwiseempire1@gmail.com>
2026-08-18 08:44:17 +0000

    _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-  }
-  
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        {/* Chargement de Tailwind CSS via CDN pour appliquer tout le design */}
+        <script src="https://cdn.tailwindcss.com"></script>
+      </Head>
+      <body className="bg-[#090d16] text-slate-100">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
